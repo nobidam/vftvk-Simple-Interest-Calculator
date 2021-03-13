8280bc4377bc512ebaf6c2351ca7884f701a3e61
nobidam <75511785+nobidam@users.noreply.github.com>
2021-03-13 08:12:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    r = document.getElementById("rate").value;
+    y = document.getElemenrById("years").value;
     
 }
         
