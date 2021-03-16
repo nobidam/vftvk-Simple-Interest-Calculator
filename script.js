79bf632c63f28636dd5de3d9b95ac4876e3d7dd3
nobidam <75511785+nobidam@users.noreply.github.com>
2021-03-16 08:13:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    y = document.getElemenrById("years").value;
-    
+unction compute() {
+
+
+  var p = +document.getElementById("principal").value;
+  var r = +document.getElementById("rate").value;
+  var startYear = 2020;
+  var t = +document.getElementById("years").value;
+  var endYear = startYear + t;
+  var Amount = p * (1 + ((r/100) * t));
+
+  var StatementAText = "If you deposit " + p;
+  var StatementBText = "On an interest of " + r + "%";
+  var StatementCText = "You will receive an amount of $" + Amount;
+  var StatementDText = "In the year " + endYear;
+
+
+
+    document.getElementById("statementA").innerHTML = StatementAText;
+    document.getElementById("statementB").innerHTML = StatementBText;
+    document.getElementById("statementC").innerHTML = StatementCText;
+    document.getElementById("statementD").innerHTML = StatementDText;
+
 }
+
         
