7f12f77ec8f5c49703618a335c2f359abbcdffcc
nobidam <75511785+nobidam@users.noreply.github.com>
2021-03-20 08:17:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -13,27 +13,15 @@ function compute() {
   var StatementCText = "You will receive an amount of $" + Amount;
   var StatementDText = "In the year " + endYear;
 
-
-
+ // If x is Not a Number or less than one
+  if (isNaN(x) || x < 1) {
+    text = "Input not valid";
+  } else {
+      text = "Input OK";
     document.getElementById("statementA").innerHTML = StatementAText;
     document.getElementById("statementB").innerHTML = StatementBText;
     document.getElementById("statementC").innerHTML = StatementCText;
     document.getElementById("statementD").innerHTML = StatementDText;
 
-}
-
-function validate() {
-  var x, text;
-
-  // Get the value of the input field with id="principal"
-  x = document.getElementById(“principal”).value;
-
-  // If x is Not a Number or less than one or greater than 10
-  if (isNaN(x) || x < 1) {
-    text = "Input not valid";
-  } else {
-    text = "Input OK";
-  }
-  document.getElementById("principal").innerHTML = text;
 }
 
